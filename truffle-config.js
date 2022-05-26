a6c6a06a29362e05df43ca3665b7a9bb251040c0
pjh94 <andy3638@naver.com>
2022-05-26 19:03:11 +0900

    truffle-config rinkeby update

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,31 @@
 const path = require("path");
 
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const mnemonic = "orange apple banana ... ";
+// or
+const privateKey = "....";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
-      port: 8545
-    }
-  }
+      port: 8545,
+    },
+
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(
+          privateKey,
+          // or mnemonic,
+          "https://rinkeby.infura.io/v3/<INFURA_Access_Token>"
+        );
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    },
+  },
 };
