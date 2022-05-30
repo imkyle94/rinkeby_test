6d26d18cff7fa4be79b51f7d1a633c2808cb6803
imkyle94 <andy3638@naver.com>
2022-05-30 11:17:45 +0900

    config update

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,12 @@
 const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const HDWalletProvider = require("truffle-hdwallet-provider");
 
-const mnemonic = "orange apple banana ... ";
+const mnemonic = process.env.mnemonic || "orange apple banana ... ";
 // or
-const privateKey = "....";
+const privateKey = process.env.privateKey || "abcde.....";
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -15,17 +17,34 @@ module.exports = {
       port: 8545,
     },
 
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", //match any network id
+    },
+
     rinkeby: {
       provider: function () {
         return new HDWalletProvider(
           privateKey,
           // or mnemonic,
-          "https://rinkeby.infura.io/v3/<INFURA_Access_Token>"
+          `https://rinkeby.infura.io/v3/${process.env.infuraProjectID}`
         );
       },
       network_id: 4,
       gas: 4500000,
       gasPrice: 10000000000,
+    },
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      version: "^0.8.0",
     },
   },
 };
